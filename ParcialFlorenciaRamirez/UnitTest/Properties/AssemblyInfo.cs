ff566b8544b55a7d662319972f25b7538758bb28
Florencia Ramirez <a2019110598@estudiantes.upsa.edu.bo>
2020-09-19 12:50:34 -0400

    Creacion Api y prueba unitari Get

---
+++ ParcialFlorenciaRamirez/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1e2cb3db-002e-4c06-840c-9cf07d56be20")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ParcialFlorenciaRamirez/UnitTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParcialFlorenciaRamirez.Controllers;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGetCountry()
+        {
+            //Arrange
+            CountriesController countriesController = new CountriesController();
+
+            //Act
+            var ListCountries = countriesController.GetCountries();
+
+            //Assert
+            Assert.IsNotNull(ListCountries);
+        }
+    }
+}
